14eed201f77bb90689c789d1d81be09f2a0c158f
waiter2020 <1403976416@qq.com>
2019-06-28 01:22:01 +0800

    Revert: 人脸、切割

--- src/main/java/com/upc/photo/service/impl/PhotoServiceImpl.java
+++ src/main/java/com/upc/photo/service/impl/PhotoServiceImpl.java
@@ -136,7 +136,10 @@ public class PhotoServiceImpl implements PhotoService {
     public Map<String,Long> getCityList(String userName) {
         ArrayList<Photo> all = photoDao.findAllByAuthorAndAddressNotNull(userName);
         HashSet<String> citys = new HashSet<>();
-        all.forEach(e-> citys.add(e.getAddress().getCity()));
+        for(Photo p:all){
+            citys.add(p.getAddress().getCity());
+        }
+        //all.forEach(e-> citys.add(e.getAddress().getCity()));
         Iterator<String> iterator = citys.iterator();
         HashMap<String,Long> map = new HashMap<>();
         while (iterator.hasNext()){
